cfa4138185f6ce106bf793f41e83d871d2b367e3
Furkan Memis <fmemis0160@gmail.com>
2023-06-22 19:38:19 +0300

    Test done

---
+++ CarClass/MyCar/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass.MyCar
+{
+    public class Car
+    {
+
+        private int carDoor;
+        private int carWheels;
+        private string carColor;
+
+        public Car(int carDoor, int carWheels,string carColor)
+        {
+            this.carDoor = carDoor;
+            this.carWheels = carWheels;
+            this.carColor = carColor;
+        }
+
+        public int getCarDoor()
+        {
+            return this.carDoor;
+        }
+
+        public int getCarWheels()
+        {
+            return this.carWheels;
+        }
+        
+        public string getCarColor()
+        {
+            return this.carColor;
+        }
+
+
+
+
+
+    }
+}
---
+++ CarTest/CarUnitTest/Test.cs
@@ -0,0 +1,73 @@
+﻿using CarClass.MyCar;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTest.CarUnitTest
+{
+    public class Test
+    {
+        private Car _car;
+
+        public Test()
+        {
+            _car = new Car(2, 4, "black");
+        }
+
+        [Fact]
+        public void CarClass_getCarDoor_ReturnInt()
+        {
+            //variable
+
+            //execute
+            var result = _car.getCarDoor();
+
+            //assert
+            result.Should().BeInRange(1, 5);
+
+        }
+
+        [Fact]
+        public void CarClass_getCarWheels_ReturnInt()
+        {
+            //variable
+
+            //execute
+            var result = _car.getCarWheels();
+            //assert
+
+            result.Should().Be(4);
+
+        }
+
+        [Fact]
+
+        public void CarClass_getCarColor_ReturnString()
+        {
+            //variables
+            List<string> colors = new List<string>
+            {
+                "pink",
+                "blue",
+                "black",
+                "red",
+                "white"
+            };
+
+            //execute
+
+            var result = _car.getCarColor();
+
+            //assert
+
+            result.Should().BeOneOf(colors);
+
+
+        }
+
+
+    }
+}
